820fdc7e570614c0e0dcc0cc963648cd39bf68a5
Hao Zhang <haohaozhang0128@gmail.com>
2019-02-10 16:39:33 -0600

    router

--- web/around-web/src/components/Main.js
+++ web/around-web/src/components/Main.js
@@ -2,12 +2,19 @@ import React from 'react';
 // import {Register} from './Register.js';
 import {Login} from './Login.js';
 import '../styles/Main.css';
+import { Switch, Route } from 'react-router-dom';
+import {Register} from "./Register";
 
 export class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <Login />
+                <Switch>
+                    <Route exact path="/" component={Login}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/register" component={Register}/>
+                    <Route component={Login}/>
+                </Switch>
             </div>
         );
     }
