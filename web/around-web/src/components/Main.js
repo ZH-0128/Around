8708245ea036903be257e69ae5fba84fb6298d83
Hao Zhang <haohaozhang0128@gmail.com>
2019-02-10 18:44:36 -0600

    logout

--- web/around-web/src/components/Main.js
+++ web/around-web/src/components/Main.js
@@ -1,26 +1,30 @@
 import React from 'react';
-// import {Register} from './Register.js';
 import {Login} from './Login.js';
 import '../styles/Main.css';
-import { Switch, Route } from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {Register} from "./Register";
 import {Home} from "./home";
 
 export class Main extends React.Component {
 
     getLogin = () => {
-        return this.props.isLoggedIn ? <Home/> : <Login handleSuccessfulLogin={this.props.handleSuccessfulLogin}/>;
+        return this.props.isLoggedIn ? <Redirect to="/home"/> :
+            <Login handleSuccessfulLogin={this.props.handleSuccessfulLogin}/>;
+    };
+
+    getHome = () => {
+        return this.props.isLoggedIn ? <Home/> : <Redirect to="/login"/>;
     };
 
     render() {
         return (
             <div className="main">
                 <Switch>
-                    <Route exact path="/" component={Login}/>
+                    <Route exact path="/" component={this.getLogin}/>
                     <Route path="/login" render={this.getLogin}/>
                     <Route path="/register" component={Register}/>
-                    <Route path="/home" component={Home}/>
-                    <Route component={Login}/>
+                    <Route path="/home" render={this.getHome}/>
+                    <Route component={this.getLogin}/>
                 </Switch>
             </div>
         );
