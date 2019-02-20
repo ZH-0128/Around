537d8237c058cfe20a4e01ad6e0fb022c74e5d79
Hao Zhang <haohaozhang0128@gmail.com>
2019-02-20 07:55:15 -0600

    final code

--- web/around-web/src/components/CreatePostButton.js
+++ web/around-web/src/components/CreatePostButton.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Modal, Button } from 'antd';
+import { Modal, Button, message } from 'antd';
 import { CreatePostForm } from './CreatePostForm';
+import { POS_KEY, API_ROOT, AUTH_HEADER, TOKEN_KEY } from '../constants';
 
 export class CreatePostButton extends React.Component {
   state = {
+    ModalText: 'Content of the modal',
     visible: false,
     confirmLoading: false,
   }
@@ -15,15 +17,40 @@ export class CreatePostButton extends React.Component {
   }
 
   handleOk = () => {
-    this.setState({
-      confirmLoading: true,
+    this.form.validateFields((err, values) => {
+      if (!err) {
+        console.log(values);
+        const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
+        const token = localStorage.getItem(TOKEN_KEY);
+        const formData = new FormData();
+        formData.set('lat', lat);
+        formData.set('lon', lon);
+        formData.set('message', values.message);
+        formData.set('image', values.image[0].originFileObj);
+
+        this.setState({ confirmLoading: true });
+        fetch(`${API_ROOT}/post`, {
+          method: 'POST',
+          body: formData,
+          headers: {
+            Authorization: `${AUTH_HEADER} ${token}`,
+          }
+        }).then((response) => {
+          if (response.ok) {
+            this.form.resetFields();
+            this.setState({ visible: false, confirmLoading: false });
+            return this.props.loadNearbyPosts();
+          }
+          throw new Error(response.statusText);
+        })
+            .then(() => message.success('Post created successfully!'))
+            .catch((e) => {
+              console.log(e);
+              this.setState({ confirmLoading: false });
+              message.error('Failed to create the post.');
+            });
+      }
     });
-    setTimeout(() => {
-      this.setState({
-        visible: false,
-        confirmLoading: false,
-      });
-    }, 2000);
   }
 
   handleCancel = () => {
@@ -33,6 +60,10 @@ export class CreatePostButton extends React.Component {
     });
   }
 
+  getFormRef = (formInstance) => {
+    this.form = formInstance;
+  }
+
   render() {
     const { visible, confirmLoading } = this.state;
     return (
@@ -40,15 +71,14 @@ export class CreatePostButton extends React.Component {
           <Button type="primary" onClick={this.showModal}>
             Create New Post
           </Button>
-          <Modal
-              title="Create New Post"
-              visible={visible}
-              onOk={this.handleOk}
-              okText="Create"
-              confirmLoading={confirmLoading}
-              onCancel={this.handleCancel}
+          <Modal title="Create New Post"
+                 visible={visible}
+                 onOk={this.handleOk}
+                 okText="Create"
+                 confirmLoading={confirmLoading}
+                 onCancel={this.handleCancel}
           >
-            <CreatePostForm />
+            <CreatePostForm ref={this.getFormRef}/>
           </Modal>
         </div>
     );
--- web/around-web/src/components/CreatePostForm.js
+++ web/around-web/src/components/CreatePostForm.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { Form, Upload, Icon, Input } from 'antd';
 
+const FormItem = Form.Item;
+
 class NormalCreatePostForm extends React.Component {
+  normFile = (e) => {
+    console.log('Upload event:', e);
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e && e.fileList;
+  };
+
+  beforeUpload = () => false;
+
   render() {
     const { getFieldDecorator } = this.props.form;
     const formItemLayout = {
@@ -11,36 +23,27 @@ class NormalCreatePostForm extends React.Component {
 
     return (
         <Form>
-          <Form.Item
-              {...formItemLayout}
-              label="Message"
-          >
+          <FormItem {...formItemLayout} label="Message">
             {getFieldDecorator('message', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please input your message'
-                }
-              ]
+              rules: [{
+                required: true,
+                message: 'Please input your message',
+              }],
             })(
                 <Input placeholder="Please input your message" />
             )}
-          </Form.Item>
-          <Form.Item
-              {...formItemLayout}
-              label="Image"
-          >
+          </FormItem>
+          <FormItem {...formItemLayout} label="Image" >
             <div className="dropbox">
               {getFieldDecorator('image', {
                 valuePropName: 'fileList',
                 getValueFromEvent: this.normFile,
-                rules: [
-                  {
-                    required: true, message: "Please select an image"
-                  }
-                ]
+                rules: [{
+                  required: true,
+                  message: 'Please select an image',
+                }],
               })(
-                  <Upload.Dragger name="files">
+                  <Upload.Dragger name="files" beforeUpload={this.beforeUpload}>
                     <p className="ant-upload-drag-icon">
                       <Icon type="inbox" />
                     </p>
@@ -49,7 +52,7 @@ class NormalCreatePostForm extends React.Component {
                   </Upload.Dragger>
               )}
             </div>
-          </Form.Item>
+          </FormItem>
         </Form>
     );
   }
